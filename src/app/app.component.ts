230abd7ff144f4598d48006a23401bb73af65c7f
Bruno Fernandes <bruno_ffc@outlook.com>
2024-03-13 10:19:35 -0300

    implementacao toast module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,18 @@
-
 import { Component, OnInit } from '@angular/core';
 
 import { RouterOutlet } from '@angular/router';
-import{ PrimeNGConfig } from 'primeng/api';
-
-
+import { MessageService, PrimeNGConfig } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-
-
-    ],
+  imports: [RouterOutlet, ToastModule],
+  providers: [MessageService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'stock-control';
 
   constructor(private primeNgConfig: PrimeNGConfig) {}
@@ -26,5 +21,3 @@ export class AppComponent implements OnInit{
     this.primeNgConfig.ripple = true;
   }
 }
-
-
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,23 +1,19 @@
-
 import { Component } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+
 import { ToastModule } from 'primeng/toast';
-import { FormBuilder, NgModel, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CookieService } from 'ngx-cookie-service';
 
 import { UserService } from './../../services/user/user.service';
 import { SignupUserResponse } from '../../models/interfaces/SignupUserResponse';
 import { SignupUserRequest } from '../../models/interfaces/user/SignupUserRequest';
 import { AuthRequest } from '../../models/interfaces/user/auth/AuthRequest';
-
-
-
-
-
-
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-home',
@@ -30,64 +26,89 @@ import { AuthRequest } from '../../models/interfaces/user/auth/AuthRequest';
     ButtonModule,
     ToastModule,
   ],
-  providers: [BrowserAnimationsModule, CookieService,],
+  providers: [BrowserAnimationsModule, CookieService, MessageService],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   loginCard = true;
 
   loginForm = this.formBuilder.group({
-
     email: ['', Validators.required],
     password: ['', Validators.required],
-  })
+  });
 
   signupForm = this.formBuilder.group({
-
     name: ['', Validators.required],
     email: ['', Validators.required],
     password: ['', Validators.required],
-  })
+  });
 
   constructor(
-     private formBuilder: FormBuilder,
-     private UserService: UserService,
-     private cookieService: CookieService,
-
-     ) { }
+    private formBuilder: FormBuilder,
+    private UserService: UserService,
+    private cookieService: CookieService,
+    private messageService: MessageService
+  ) {}
 
   onSubmitLoginForm(): void {
-    if(this.loginForm.value && this.loginForm.valid) {
+    if (this.loginForm.value && this.loginForm.valid) {
       this.UserService.authUser(this.loginForm.value as AuthRequest).subscribe({
         next: (response) => {
-          if(response) {
+          if (response) {
             this.cookieService.set('USER_INFO', response?.token);
-
             this.loginForm.reset();
+
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Sucesso',
+              detail: 'Bem-vindo de volta ${response.name}!',
+              life: 2000,
+            });
           }
         },
-        error: (err) => console.log(err),
+        error: (err) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao fazer login!',
+            life: 2000,
+          });
+          console.log(err);
+        },
       });
     }
-
   }
-
 
   onSubmitSignupForm(): void {
     // Verifica se o formulário de registro (signupForm) existe e se é válido
-    if(this.signupForm.value && this.signupForm.valid) {
-        // Chama um serviço chamado UserService para realizar o registro do usuário
-    // O valor do formulário (signupForm.value) é passado como argumento para a função signupUser()
-      this.UserService.signupUser(this.signupForm.value as SignupUserRequest).subscribe({
-        next:(response) => {
+    if (this.signupForm.value && this.signupForm.valid) {
+      // Chama um serviço chamado UserService para realizar o registro do usuário
+      // O valor do formulário (signupForm.value) é passado como argumento para a função signupUser()
+      this.UserService.signupUser(
+        this.signupForm.value as SignupUserRequest
+      ).subscribe({
+        next: (response) => {
           if (response) {
-            alert('Usuário teste  criado com sucesso!');
             this.signupForm.reset();
             this.loginCard = true;
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Sucesso',
+              detail: 'Usuário criado com sucesso!',
+              life: 2000,
+            });
           }
-      },
-      error:(err) => console.log(err),
+        },
+        error: (err) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao criar usuário!',
+            life: 2000,
+          });
+          console.log(err);
+        },
       });
     }
   }
