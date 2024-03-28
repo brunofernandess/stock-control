c5e266354b9fcc720676d637d87f5ab8cf1f111d
brunofernandess <bruno_ffc@outlook.com>
2024-03-28 11:34:31 -0300

    adicionando rotas e tela de produtos

---
+++ src/modules/products/page/products-home/products-home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { ToolbarNavigationComponent } from '../../../../app/shared/componentshared/toolbar-navigation/toolbar-navigation.component';
+
+
+@Component({
+  selector: 'app-products-home',
+  standalone: true,
+  imports: [ToolbarNavigationComponent],
+  templateUrl: './products-home.component.html',
+  styleUrls: []
+})
+export class ProductsHomeComponent {
+
+
+}
